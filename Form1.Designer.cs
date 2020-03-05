df69c36132b7d70f7c64c467a75387003a43c509
tompedro <55707296+tompedro@users.noreply.github.com>
2020-03-05 11:12:42 +0100

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace STARK___Compiler_and_Debugger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.code = new System.Windows.Forms.RichTextBox();
+            this.python = new System.Windows.Forms.RichTextBox();
+            this.debug = new System.Windows.Forms.ListBox();
+            this.compileBTN = new System.Windows.Forms.Button();
+            this.runBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // code
+            // 
+            this.code.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.code.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(36)))), ((int)(((byte)(36)))));
+            this.code.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.code.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.code.ForeColor = System.Drawing.SystemColors.Control;
+            this.code.Location = new System.Drawing.Point(12, 12);
+            this.code.Name = "code";
+            this.code.Size = new System.Drawing.Size(740, 426);
+            this.code.TabIndex = 0;
+            this.code.Text = "";
+            this.code.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
+            // 
+            // python
+            // 
+            this.python.AccessibleName = "python";
+            this.python.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.python.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(36)))), ((int)(((byte)(36)))));
+            this.python.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.python.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.python.ForeColor = System.Drawing.SystemColors.Control;
+            this.python.Location = new System.Drawing.Point(758, 12);
+            this.python.Name = "python";
+            this.python.Size = new System.Drawing.Size(553, 426);
+            this.python.TabIndex = 1;
+            this.python.Text = "";
+            // 
+            // debug
+            // 
+            this.debug.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.debug.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.debug.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.debug.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.debug.ForeColor = System.Drawing.SystemColors.Control;
+            this.debug.FormattingEnabled = true;
+            this.debug.ItemHeight = 18;
+            this.debug.Location = new System.Drawing.Point(12, 496);
+            this.debug.Name = "debug";
+            this.debug.Size = new System.Drawing.Size(1299, 108);
+            this.debug.TabIndex = 2;
+            // 
+            // compileBTN
+            // 
+            this.compileBTN.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.compileBTN.BackColor = System.Drawing.Color.Maroon;
+            this.compileBTN.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.compileBTN.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.compileBTN.Location = new System.Drawing.Point(12, 444);
+            this.compileBTN.Name = "compileBTN";
+            this.compileBTN.Size = new System.Drawing.Size(740, 36);
+            this.compileBTN.TabIndex = 3;
+            this.compileBTN.Text = "Compile";
+            this.compileBTN.UseVisualStyleBackColor = false;
+            this.compileBTN.Click += new System.EventHandler(this.CompileBTN_Click);
+            // 
+            // runBTN
+            // 
+            this.runBTN.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.runBTN.BackColor = System.Drawing.Color.ForestGreen;
+            this.runBTN.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.runBTN.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.runBTN.Location = new System.Drawing.Point(758, 444);
+            this.runBTN.Name = "runBTN";
+            this.runBTN.Size = new System.Drawing.Size(553, 36);
+            this.runBTN.TabIndex = 4;
+            this.runBTN.Text = "Run";
+            this.runBTN.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1323, 625);
+            this.Controls.Add(this.runBTN);
+            this.Controls.Add(this.compileBTN);
+            this.Controls.Add(this.debug);
+            this.Controls.Add(this.python);
+            this.Controls.Add(this.code);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox code;
+        private System.Windows.Forms.RichTextBox python;
+        private System.Windows.Forms.ListBox debug;
+        private System.Windows.Forms.Button compileBTN;
+        private System.Windows.Forms.Button runBTN;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace STARK___Compiler_and_Debugger
+{
+    public partial class Form1 : Form
+    {
+        public int dimensions;
+        public List<string> names;
+        public bool isClass = false;
+        public int nInd;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            HighlightText("#DEFINE", Color.Orange);
+            HighlightText("ARG", Color.Orange);
+            HighlightText("act", Color.DeepSkyBlue);
+            HighlightText("@init", Color.DarkGreen);
+            HighlightText("@loop", Color.DarkGreen);
+            HighlightText("@func", Color.DarkGreen);
+            HighlightText("if", Color.DeepPink);
+            HighlightText("for", Color.DeepPink);
+            HighlightText("while", Color.DeepPink);
+        }
+        public void HighlightText(string word, Color color)
+        {
+
+            if (word == string.Empty)
+                return;
+
+            int s_start = code.SelectionStart, startIndex = 0, index;
+
+            while ((index = code.Text.IndexOf(word, startIndex)) != -1)
+            {
+                code.Select(index, word.Length);
+                code.SelectionColor = color;
+
+                startIndex = index + word.Length;
+            }
+
+            code.SelectionStart = s_start;
+            code.SelectionLength = 0;
+            code.SelectionColor = Color.White;
+        }
+        
+        private void CompileBTN_Click(object sender, EventArgs e)
+        {
+            python.Text = "";
+            nInd = 0;
+            foreach(String stringLine in code.Lines)
+            {
+                string s = stringLine;
+                if (s.Contains("#DEFINE"))
+                {
+                    if (s.Contains("DIMENSIONS") && s.Contains("="))
+                    {
+                        s = stringLine.Replace(" ", "");
+                        string sub = s.Substring(s.IndexOf("=") + 1, s.Length - s.IndexOf("=") - 1);
+                        if (sub.Replace(" ", "") != "")
+                        {
+                            dimensions = Int32.Parse(s.Substring(s.IndexOf("=") + 1, s.Length - s.IndexOf("=") - 1));
+                            nInd = 0;
+                            writePython("dimensions = " + dimensions.ToString() + "\n");
+                        }
+                    }else if(s.Contains("NAME") && s.Contains("=") && s.Contains("ARG") && s.Contains("(") && s.Contains(")"))
+                    {
+                        string sub = s.Substring(s.IndexOf("=") + 1, s.Length - s.IndexOf("=") - 1);
+                        string args = s.Substring(s.IndexOf("(") + 1, s.IndexOf(")") - s.IndexOf("(") - 1);
+                        string sub2 = "";
+                        if (args.Replace(" ","") != "")
+                        {
+                            
+                            foreach (string a in args.Split(','))
+                            {
+                                sub2 += a.Split(' ')[1] + ",";
+                            }
+                            sub2 = sub2.Substring(0, sub2.Length - 1);
+                            debug.Items.Add(sub2);
+                        }
+                        
+                        if (sub.Replace(" ", "") != "" && sub2.Replace(" ", "") != "")
+                        {
+                            string name = s.Substring(s.IndexOf("=") + 1, s.IndexOf("ARG") - s.IndexOf("=") - 1);
+                            nInd = 0;
+                            writePython("class " + name + "(" + sub2 + "):\n");
+                            isClass = true;
+                            writePython("def __init__(self ," + sub2 + "):\n");
+                            nInd = 1;
+                            foreach(string arg in sub2.Split(','))
+                            {
+                                writePython("self." + arg + "=" + arg);
+                            }
+                            nInd = 0;
+                        }
+                    }
+                }else if (s.Contains("act"))
+                {
+                    string name = s.Substring(s.IndexOf("t") + 1, s.IndexOf("@") - s.IndexOf("t") - 1);
+                    string type = s.Substring(s.IndexOf("@") + 1, s.IndexOf("(") - s.IndexOf("@") - 1);
+                    string rawArgs = s.Substring(s.IndexOf("(") + 1, s.IndexOf(")") - s.IndexOf("(") - 1);string args = "";
+                    if(rawArgs != "")
+                    {
+                        args = "";
+                        foreach (string a in rawArgs.Split(','))
+                        {
+                            args += a.Split(' ')[1] + ",";
+                        }
+                        args = args.Substring(0, args.Length - 1);
+                    }
+                    
+                    if (type == "init")
+                    {
+                        writePython("def " + name + "():");
+                        if (isClass)
+                        {
+                            bool found = false;
+                            int i = 0;
+                            foreach(string line in python.Lines)
+                            {
+                                if (line.Contains("__init__"))
+                                {
+                                    found = true;
+                                }
+                                else if(found && line.Contains("def"))
+                                {
+                                    debug.Items.Add(i.ToString());
+                                    nInd++;
+                                    python.Text = python.Text.Insert(i,"\n" +  name + "()");
+                                    nInd--;
+                                }
+                                i += line.Length;
+                            }
+                        }
+                        else
+                        {
+                            writePython(name + "()");
+                        }
+                    }
+                    else if(type == "loop")
+                    {
+                        writePython("def " + name + "():");
+                        if (isClass)
+                        {
+                            bool found = false;
+                            int i = 0;
+                            foreach (string line in python.Lines)
+                            {
+                                if (line.Contains("__init__"))
+                                {
+                                    found = true;
+                                }
+                                else if (found && line.Contains("def"))
+                                {
+                                    debug.Items.Add(i.ToString());
+                                    nInd++;
+                                    python.Text = python.Text.Insert(i, "\n while True:");
+                                    nInd++;
+                                    python.Text = python.Text.Insert(i, "\n" + name + "()");
+                                    nInd -= 2;
+                                }
+                                i += line.Length;
+                            }
+                        }
+                        else
+                        {
+                            writePython("while True:");
+                            nInd++;
+                            writePython(name + "()");
+                            nInd--;
+                        }
+                    }else if(type == "func")
+                    {
+                        writePython("def " + name + "(" + args + "):");
+                    }
+                    nInd++;
+                }else if (s.Contains("}"))
+                {
+                    nInd--;
+                }
+                else if (s.Contains("if"))
+                {
+                    string str ="";
+                    foreach(char c in s)
+                    {
+                        if(c =='(' || c== ')'){
+                            str += " ";
+                        }else if(c == '£')
+                        {
+                            str += "and";
+                        }
+                        else if (c == '/')
+                        {
+                            str += "or";
+                        }
+                        else
+                        {
+                            str += c;
+                        }
+                    }
+                    str += ":";
+                    writePython(str);
+                    nInd++;
+                }
+                else if(!s.Contains("{"))
+                {
+                    writePython(s.Replace(";",""));
+
+                }
+            }
+        }
+
+        public void writePython(string line)
+        {
+            if (isClass)
+            {
+                line = "    " + line;
+            }
+             if (nInd != 0)
+            {
+                int k = nInd;
+                while(k > 0)
+                {
+                    line = "    " + line;
+                    k--;
+                }    
+            }
+            //debug.Items.Add(nInd.ToString());
+            python.AppendText(line + "\n");
+        }
+    }
+}
